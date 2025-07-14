98ad07f8b41a05afe69f6937c407e6e68a500708
Scott McPeak <scott.g.mcpeak@gmail.com>
2025-07-14 00:48:03 -0700

    Add boilerplate.py module.
    empty-header, format-header-comments.py: Use `boilerplate`.
    
    Adapting other scripts is future work.

--- format-header-comments.py
+++ format-header-comments.py
@@ -16,66 +16,15 @@ with all of the comments aligned to a column that is a multiple of 10.
 """
 
 import argparse              # argparse
-import os                    # os.getenv
 import re                    # re.compile
-import signal                # signal.signal
-import sys                   # sys.argv, sys.stderr, sys.stdin
+import sys                   # sys.stdin
 import traceback             # traceback.print_exc
 
-from typing import Any, Match, Pattern, TextIO
+from typing import Any, Match, Pattern, TextIO, Union
+from boilerplate import *
 
 
-# -------------- BEGIN: boilerplate -------------
-# These are things I add at the start of every Python program to
-# allow better error reporting.
-
-# Positive if debug is enabled, with higher values enabling more printing.
-debugLevel = 0
-if debugEnvVal := os.getenv("DEBUG"):
-  debugLevel = int(debugEnvVal)
-
-def debugPrint(str: str) -> None:
-  """Debug printout when DEBUG >= 2."""
-  if debugLevel >= 2:
-    print(str)
-
-# Ctrl-C: interrupt the interpreter instead of raising an exception.
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-class Error(Exception):
-  """A condition to be treated as an error."""
-  pass
-
-def die(message: str) -> None:
-  """Throw a fatal Error with message."""
-  raise Error(message)
-
-def exceptionMessage(e: BaseException) -> str:
-  """Turn exception 'e' into a human-readable message."""
-  t = type(e).__name__
-  s = str(e)
-  if s:
-    return f"{t}: {s}"
-  else:
-    return f"{t}"
-
-def call_main() -> None:
-  """Call main() and catch exceptions."""
-  try:
-    main()
-
-  except SystemExit as e:
-    raise      # Let this one go, otherwise sys.exit gets "caught".
-
-  except BaseException as e:
-    print(f"{exceptionMessage(e)}", file=sys.stderr)
-    if (debugLevel >= 1):
-      traceback.print_exc(file=sys.stderr)
-    sys.exit(2)
-# --------------- END: boilerplate --------------
-
-
-def main():
+def main() -> None:
   # Parse command line.
   parser = argparse.ArgumentParser(
     description="Read in a set of #includes, write it out with "+
@@ -83,16 +32,16 @@ def main():
   opts = parser.parse_args()
 
   # All input lines.
-  inputLines = sys.stdin.readlines()
+  inputLines: list[str] = sys.stdin.readlines()
 
   # Length of the longest code line, ignoring trailing whitespace and
   # comment.  The initial value is the minimum column number.
-  longestLength = 40
+  longestLength: int = 40
 
   # For each input line, either a tuple of (code, comment) or a string
   # that is just the code (which could itself be a comment, if the input
   # line only has a comment).
-  parsedLines = []
+  parsedLines: list[Union[tuple[str, str], str]] = []
 
   # A line of code to format should have:
   #   1. Something that ends with non-whitespace.
@@ -106,8 +55,8 @@ def main():
     line = line.rstrip("\r\n\t ")
 
     if m := codeLineRE.match(line):
-      code = m.group(1)
-      comment = m.group(3)
+      code: str = m.group(1)
+      comment: str = m.group(3)
 
       # Add to the longest length, using +2 because (+1) we want at
       # least one space between the code and the comment, and (+1) to
@@ -135,7 +84,8 @@ def main():
       print(f"{code}{padding}{comment}")
 
 
-call_main()
+if __name__ == "__main__":
+  call_main(main)
 
 
 # EOF
