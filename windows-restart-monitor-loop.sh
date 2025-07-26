c2cfe39cfa6197c65fc3c8a94a11aa8825b6a4cf
Scott McPeak <scott.g.mcpeak@gmail.com>
2025-07-26 06:45:59 -0700

    Add: windows-restart-monitor-loop.sh.

---
+++ windows-restart-monitor-loop.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Run `windows-restart-monitor.py` in a loop.
+
+if [ "x$2" = "x" ]; then
+  printf "usage: $0 <python program> <seconds between queries> <warn hours>\n"
+  exit 2
+fi
+
+# This requires the python program as an argument because the python on
+# the PATH is probably Cygwin Python, but the Python script to run
+# requires Windows Python.
+python="$1"
+
+seconds="$2"
+hours="$3"
+
+monitor=$(cygpath -m $(which windows-restart-monitor.py))
+
+while true; do
+  echo "$python" "$monitor" -warn "$hours"
+  "$python" "$monitor" -warn "$hours"
+  sleep "$seconds"
+done
+
+# EOF
