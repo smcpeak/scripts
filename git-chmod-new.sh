be5eb6283336261506c69948a75399417b3521d2
Scott McPeak <scott.g.mcpeak@gmail.com>
2025-09-27 19:47:44 -0700

    git-chmod-new.sh: Add extensions pyi and ini.

--- git-chmod-new.sh
+++ git-chmod-new.sh
@@ -6,7 +6,7 @@
 # default.
 
 git ls-files --others --exclude-standard | \
-  egrep -i '\.(h[hp]*|c[cp]*|ded|png|ev|json|gdvn|txt)$' | \
+  egrep -i '\.(h[hp]*|c[cp]*|ded|png|ev|json|gdvn|txt|pyi|ini)$' | \
   xargsn runecho chmod 644
 
 # EOF
