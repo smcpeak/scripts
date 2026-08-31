93744398ed0506e54270815b0c80ddd074ce91e1
Scott McPeak <scott.g.mcpeak@gmail.com>
2026-08-30 18:54:05 -0700

    git-chmod-new.sh: Add "lex" extension.

--- git-chmod-new.sh
+++ git-chmod-new.sh
@@ -6,7 +6,7 @@
 # default.
 
 git ls-files --others --exclude-standard | \
-  egrep -i '\.(h[hp]*|c[cp]*|ded|png|ev|json|gdvn|txt|pyi|ini|md|qrc|ico|jpg|xcf)|Makefile$' | \
+  egrep -i '\.(h[hp]*|c[cp]*|ded|png|ev|json|gdvn|txt|pyi|ini|md|qrc|ico|jpg|xcf|lex)|Makefile$' | \
   xargsn runecho chmod 644
 
 # EOF
